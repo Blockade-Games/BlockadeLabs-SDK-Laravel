7c0984096f983a94a8de7066d5a290207e08f868
Goran P <geoligard@gmail.com>
2023-04-05 18:33:26 +0200

    Figure out how to submit files

--- src/SDKClient.php
+++ src/SDKClient.php
@@ -45,4 +45,42 @@ class SDKClient
 
         return $response->json();
     }
+
+    public function getGenerators()
+    {
+        try {
+            $response = Http::get($this->apiUrl . '/generators?api_key=' . $this->apiKey)->throw();
+        } catch (RequestException $e) {
+            $response = $e->response;
+            \Log::error('Get Generators failed: ' . $e);
+            \Log::error($e->response);
+        }
+
+        return $response->json();
+    }
+
+    public function generateImagine($params, $files = [])
+    {
+        try {
+            $response = Http::asMultipart();
+
+            foreach ($params as $key => $param) {
+                if(request()->file($key)) {
+                    $image = request()->file($key);
+                    $fileName = $image->getClientOriginalName();
+                    $response = $response->attach($key, $image->getContent(), $fileName);
+                    unset($params[$key]);
+                }
+            }
+
+            $response = $response->post($this->apiUrl . '/imagine/requests?api_key=' . $this->apiKey, $params)
+                ->throw();
+        } catch (RequestException $e) {
+            $response = $e->response;
+            \Log::error('Generate Imagine failed: ' . $e);
+            \Log::error($e->response);
+        }
+
+        return $response->json();
+    }
 }
